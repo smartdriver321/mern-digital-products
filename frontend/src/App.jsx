9644f68011571ec41638088fa39e79198403bab9
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-27 18:07:36 +0700

    Add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,16 @@ import CreateProduct from './pages/CreateProduct'
 import EditProduct from './pages/EditProduct'
 import DeleteProduct from './pages/DeleteProduct'
 import ProtectedRoutes from './components/ProtectedRoutes'
+import Navbar from './components/Navbar'
+import Cart from './pages/Cart'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
 
         <Route
           path='/admin/*'
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+
 import ProductCard from '../components/ProductCard'
 
 export default function Home() {
